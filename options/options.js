ca77903f579b20ba1197a0f2ef76a8b78de2d98d
Crowdbotics <team@crowdbotics.com>
2021-09-10 19:55:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "oktook_30449Identifier",
+  url: "https://oktook_30449Identifier.botics.co",
+  api: "https://oktook_30449Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
